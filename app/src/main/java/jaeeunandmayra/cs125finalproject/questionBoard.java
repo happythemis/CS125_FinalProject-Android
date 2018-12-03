3c9ebac3839e3e2c549401ac0d9b24f9633dbc58
happythemis <themislee1924@gmail.com>
2018-12-03 16:47:23 -0600

    prob11- edit

--- app/src/main/java/jaeeunandmayra/cs125finalproject/questionBoard.java
+++ app/src/main/java/jaeeunandmayra/cs125finalproject/questionBoard.java
@@ -7,13 +7,13 @@ import android.view.View;
 import android.widget.Button;
 
 public class questionBoard extends AppCompatActivity {
-    private Button button;
+    //private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question_board);
-
+//
 //        button = (Button) findViewById(R.id.button2);
 //        button.setOnClickListener(new View.OnClickListener() {
 //            @Override
