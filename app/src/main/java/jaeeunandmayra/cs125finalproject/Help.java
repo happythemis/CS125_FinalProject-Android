00994927e1f5d6ba3101534665a1d84a111d9e15
happythemis <themislee1924@gmail.com>
2018-12-03 15:25:14 -0600

    prob4 - move on to new activity worked,,,

--- app/src/main/java/jaeeunandmayra/cs125finalproject/Help.java
+++ app/src/main/java/jaeeunandmayra/cs125finalproject/Help.java
@@ -1,13 +1,32 @@
 package jaeeunandmayra.cs125finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Help extends AppCompatActivity {
 
+
+    public Button button1;
+    public void goBackMain() {
+        button1 = findViewById(R.id.button);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent goBack = new Intent(Help.this, MainActivity.class);
+                startActivity(goBack);
+            }
+        });
+
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_help);
+        goBackMain();
+
     }
 }
