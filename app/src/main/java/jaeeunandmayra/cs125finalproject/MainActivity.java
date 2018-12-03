73f7351b693017ec8380fb390393370f53f9df85
happythemis <themislee1924@gmail.com>
2018-12-03 14:46:23 -0600

    prob3

--- app/src/main/java/jaeeunandmayra/cs125finalproject/MainActivity.java
+++ app/src/main/java/jaeeunandmayra/cs125finalproject/MainActivity.java
@@ -1,19 +1,38 @@
 package jaeeunandmayra.cs125finalproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    public Button button1;
+    public void goHelp() {
+        button1 = (Button)findViewById(R.id.button4);
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent playHelp = new Intent(MainActivity.this, Help.class);
+            }
+        });
+
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        goHelp();
     }
 
-    public void goGame(View view) {
-//        Log.d(LOG_TAG,  )
-
-    }
+//    public void goGame(View view) {
+////        Log.d(LOG_TAG,  )
+//
+//    }
+//    public void goHelp(View v) {
+//        Button button = (Button) v;
+//        startActivity(new Intent(getApplicationContext(), Main2Activity.class));
+//    }
 }
