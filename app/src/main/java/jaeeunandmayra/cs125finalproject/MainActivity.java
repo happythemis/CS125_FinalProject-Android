84721f16b7953cb1027383a6f37603055f7e628b
happythemis <themislee1924@gmail.com>
2018-12-03 14:15:30 -0600

    prob1

--- app/src/main/java/jaeeunandmayra/cs125finalproject/MainActivity.java
+++ app/src/main/java/jaeeunandmayra/cs125finalproject/MainActivity.java
@@ -2,6 +2,7 @@ package jaeeunandmayra.cs125finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void goGame(View view) {
+//        Log.d(LOG_TAG,  )
+
     }
 }
