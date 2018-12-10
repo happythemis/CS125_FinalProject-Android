f58a372cbe0b2bab9b5fd6d295fab43c4058b69f
happythemis <themislee1924@gmail.com>
2018-12-10 02:16:20 -0600

    prob15 - call api

--- app/src/main/java/jaeeunandmayra/cs125finalproject/MainActivity.java
+++ app/src/main/java/jaeeunandmayra/cs125finalproject/MainActivity.java
@@ -1,10 +1,23 @@
 package jaeeunandmayra.cs125finalproject;
 
+import android.app.VoiceInteractor;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONObject;
+
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity  implements View.OnClickListener {
 
@@ -30,6 +43,27 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 
         buttonHelp.setOnClickListener(this);
         buttonStart.setOnClickListener(this);
+
+
+        String URL = "https://opentdb.com/api.php?amount=16&difficulty=easy&type=multiple";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                Log.e("Rest Response", response.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Rest Response", error.toString());
+            }
+        }
+        );
+
+        requestQueue.add(objectRequest);
 
 
     }
