1a57a19079807109b0a13e84bb82150dc6af2bf6
Mayrat24v24 <mayrat24v24@gmail.com>
2018-12-10 17:26:46 -0600

    Correct answer shows

--- app/src/main/java/jaeeunandmayra/cs125finalproject/Answer.java
+++ app/src/main/java/jaeeunandmayra/cs125finalproject/Answer.java
@@ -2,12 +2,72 @@ package jaeeunandmayra.cs125finalproject;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Answer extends AppCompatActivity {
 
+    TextView viewAnswer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer);
+
+
+        viewAnswer = findViewById(R.id.textView5);
+
+
+
+
+
+        String URL = "https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    //JSONObject main = response.getJSONObject("question");
+                    JSONArray arr = response.getJSONArray("results");
+                    JSONObject obj = arr.getJSONObject(0);
+                    String answer = obj.getString("correct_answer");
+
+                    viewAnswer.setText(answer);
+
+
+
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Log.e("Rest Response", response.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Rest Response", error.toString());
+            }
+        }
+        );
+
+
+        //requestQueue.add(objectRequest);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(objectRequest);
     }
 }
