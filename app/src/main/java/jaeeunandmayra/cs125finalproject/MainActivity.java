8eb4c771a9cfbf235d8730b1fd8a875fedab0b9f
happythemis <themislee1924@gmail.com>
2018-12-10 16:23:20 -0600

    prob15 - call api and show in text basics lvl2

--- app/src/main/java/jaeeunandmayra/cs125finalproject/MainActivity.java
+++ app/src/main/java/jaeeunandmayra/cs125finalproject/MainActivity.java
@@ -28,7 +28,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
 
     Button buttonStart, buttonHelp;
     //TextView viewQ;
-    TextView viewQuestion;
+
     public void onClick(View v){
         if (v.getId() == R.id.button3) {
             Intent goStart = new Intent(MainActivity.this, Board.class);
@@ -51,50 +51,7 @@ public class MainActivity extends AppCompatActivity  implements View.OnClickList
         buttonHelp.setOnClickListener(this);
         buttonStart.setOnClickListener(this);
 
-        viewQuestion = findViewById(R.id.textView4);
 
-
-
-
-
-        String URL = "https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple";
-
-        RequestQueue requestQueue = Volley.newRequestQueue(this);
-        JsonObjectRequest objectRequest = new JsonObjectRequest(
-                Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
-            @Override
-            public void onResponse(JSONObject response) {
-                try {
-                    //JSONObject main = response.getJSONObject("question");
-                    JSONArray arr = response.getJSONArray("results");
-                    JSONObject obj = arr.getJSONObject(0);
-                    String question = obj.getString("question");
-
-                    viewQuestion.setText(question);
-
-
-
-
-
-
-                } catch (JSONException e) {
-                    e.printStackTrace();
-                }
-                Log.e("Rest Response", response.toString());
-
-            }
-        }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.e("Rest Response", error.toString());
-            }
-        }
-        );
-
-
-        //requestQueue.add(objectRequest);
-        RequestQueue queue = Volley.newRequestQueue(this);
-        queue.add(objectRequest);
 
 
 
--- app/src/main/java/jaeeunandmayra/cs125finalproject/Question.java
+++ app/src/main/java/jaeeunandmayra/cs125finalproject/Question.java
@@ -3,12 +3,26 @@ package jaeeunandmayra.cs125finalproject;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonObjectRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Question extends AppCompatActivity {
 
-    public Button buttonGoMain;
+    Button buttonGoMain;
+    TextView viewQuestion;
     public void goBackBoard() {
         buttonGoMain = findViewById(R.id.button23);
         buttonGoMain.setOnClickListener(new View.OnClickListener() {
@@ -26,5 +40,50 @@ public class Question extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_question);
         goBackBoard();
+
+        viewQuestion = findViewById(R.id.textView6);
+
+
+
+
+
+        String URL = "https://opentdb.com/api.php?amount=1&difficulty=easy&type=multiple";
+
+        RequestQueue requestQueue = Volley.newRequestQueue(this);
+        JsonObjectRequest objectRequest = new JsonObjectRequest(
+                Request.Method.GET, URL, null, new Response.Listener<JSONObject>() {
+            @Override
+            public void onResponse(JSONObject response) {
+                try {
+                    //JSONObject main = response.getJSONObject("question");
+                    JSONArray arr = response.getJSONArray("results");
+                    JSONObject obj = arr.getJSONObject(0);
+                    String question = obj.getString("question");
+
+                    viewQuestion.setText(question);
+
+
+
+
+
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+                Log.e("Rest Response", response.toString());
+
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Log.e("Rest Response", error.toString());
+            }
+        }
+        );
+
+
+        //requestQueue.add(objectRequest);
+        RequestQueue queue = Volley.newRequestQueue(this);
+        queue.add(objectRequest);
     }
 }
